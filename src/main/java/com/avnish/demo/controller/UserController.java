18c764e6454776672709f56fc54b64d200abb970
Avnish Kumar <Avnein4988@gmail.com>
2024-06-29 20:13:15 +0530

    implement the user service(some endpoints still remained to implement in user controller

---
+++ src/main/java/com/avnish/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.avnish.demo.controller;
+
+import com.avnish.demo.modal.User;
+import com.avnish.demo.service.EmailService;
+import com.avnish.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt) throws  Exception
+    {
+        User user=userService.findUserProfileByJwt(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/enable-two-factor/verify-otp/{otp}")
+    public ResponseEntity<User> enableTwoFactorAuthentication(@RequestHeader("Authorization") String jwt) throws  Exception
+    {
+        User user=userService.findUserProfileByJwt(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+
+
+}
--- src/main/java/com/avnish/demo/service/UserService.java
+++ src/main/java/com/avnish/demo/service/UserService.java
@@ -1,13 +1,75 @@
 package com.avnish.demo.service;
 
+import com.avnish.demo.config.JwtProvider;
+import com.avnish.demo.domain.VerificationType;
+import com.avnish.demo.modal.TwofactorAuth;
+import com.avnish.demo.modal.User;
 import com.avnish.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    public User findUserProfileByJwt(String jwt) throws  Exception
+    {
+        String email= JwtProvider.getEmailFromToken(jwt);
+        User user=userRepository.findByEmail(email);
+
+        if(user==null)
+        {
+            throw new Exception("User not found");
+        }
+
+        return user;
+    }
+
+    public User findUserByEmail(String email) throws  Exception
+    {
+
+        User user=userRepository.findByEmail(email);
+
+        if(user==null)
+        {
+            throw new Exception("User not found");
+        }
+
+        return user;
+    }
+
+    public User findUserByid(Long userId) throws  Exception
+    {
+        Optional<User> user=userRepository.findById(userId);
+
+        if(user.isEmpty())
+        {
+            throw new Exception("User not found");
+        }
+
+        return user.get();
+
+    }
+
+    public User enableTwoFactorAuthentication(User user, VerificationType verificationType)
+    {
+        TwofactorAuth twofactorAuth=new TwofactorAuth();
+        twofactorAuth.setEnabled(true);
+        twofactorAuth.setSendTo(verificationType);
+
+        user.setTwofactorAuth(twofactorAuth);
+
+        return userRepository.save(user);
+    }
+
+    public User updatePassword(User user,String password)
+    {
+        user.setPassword(password);
+        return userRepository.save(user);
+    }
 
 }
